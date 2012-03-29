9afcd295d3bacc714f85942bab42933aa51a25cc
Sven-S. Porst <porst@sub.uni-goettingen.de>
2012-03-29 13:20:44 +0200

    configure more services

--- pazpar2-access-configuration.php
+++ pazpar2-access-configuration.php
@@ -1,29 +1,67 @@
 <?php
 
 $serviceConfig = Array(
-	'AAC' => Array (
+	'AAC' => Array(
 		'sru.gbv.de/opac-de-7' => 'default',
 		'sru.gbv.de/zdb-1-pio' => 'default',
 	),
-	'SUB' => Array(
+	'AAC-Neuerwerbungen' => Array(
+		'sru.gbv.de/opac-de-7' => 'default',
+	),
+	'AAC-Hist-Themen' => Array(
+		'sru.gbv.de/opac-de-7' => 'default',
+	),
+	'AAC-Lit-Themen' => Array(
+		'sru.gbv.de/opac-de-7' => 'default',
+	),
+	'GEO-LEO' => Array(
+		'sru.gbv.de/opac-de-7' => 'default',
+		'z3950.bsz-bw.de:20215/swb' => 'default',
+	),
+	'GEO-LEO-Themen' => Array(
+		'sru.gbv.de/opac-de-7' => 'default',
+		'z3950.bsz-bw.de:20215/swb' => 'default',
+	),
+	'Math' => Array(
+		'sru.gbv.de/opac-de-7' => 'default',
+	),
+	'Math-Neuerwerbungen' => Array(
+		'sru.gbv.de/opac-de-7' => 'default',
+	),
+	'Neuerwerbungen' => Array(
 		'sru.gbv.de/opac-de-7' => Array(
-			'catalogueURLPrefix' => Array(
+			'catalogueURLHintPrefix' => Array(
 				'value' => 'https://opac.sub.uni-goettingen.de/DB=1/PPNSET?PPN=',
 			),
 		),
 	),
-	'all' => Array (
+	'all' => Array(
+		'sru.gbv.de/opac-de-7' => 'default',
+		'sru.gbv.de/zdb-1-pio' => 'default',
+		'sru.gbv.de/olc' => 'default',
+		'sru.gbv.de/olcssg-ang' => 'default',
+		'sru.gbv.de/olcssg-his' => 'default',
+		'sru.gbv.de/olcssg-mat' => 'default',
+		'sru.gbv.de/olcssg-ggo' => 'default',
+		'sru.gbv.de/olcssg-ast' => 'default',
 	)
 );
 
 
 $databaseDefaults = Array(
 	'sru.gbv.de/opac-de-7' => Array(
-		'catalogueURLPrefix' => Array(
+		'catalogueURLHintPrefix' => Array(
 			Array(
 				'conditionType' => 'IP',
 				'condition' => Array('134.76.*'),
 				'value' => 'https://opac.sub.uni-goettingen.de/DB=1/PPNSET?PPN=',
+			),
+		),
+		'pz:name' => Array(
+			Array(
+				'conditionType' => 'IP',
+				'condition' => Array('134.76.*'),
+				'value' => 'SUB Göttingen',
 			),
 		),
 	),
@@ -87,6 +125,22 @@ $databaseDefaults = Array(
 				'conditionType' => 'GBV',
 				'condition' => '5.55',
 				'value' => '1',
+			),
+		),
+	),
+	'z3950.bsz-bw.de:20215/swb' => Array(
+		'catalogueURLHintPrefix' => Array(
+			Array(
+				'conditionType' => 'IP',
+				'condition' => Array('139.20.*'),
+				'value' => 'http://webopac.ub.tu-freiberg.de/libero/WebopacOpenURL.cls?ACTION=DISPLAY&amp;sid=Libero:TUF&amp;RID=',
+			),
+		),
+		'pz:name' => Array(
+			Array(
+				'conditionType' => 'IP',
+				'condition' => Array('139.20.*'),
+				'value' => 'Freiberg',
 			),
 		),
 	),
